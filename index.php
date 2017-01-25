f7a7e0bf292b8e8466050bf316dbc05fbbe8c8c9
MikaelFeher <mikael.feher@gmail.com>
2017-01-25 12:30:14 +0100

    Adds password hashing

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 
 	$email = mysqli_real_escape_string($link, $_POST['email']);
 	$password = mysqli_real_escape_string($link, $_POST['password']);
+	$hash = password_hash($password, PASSWORD_DEFAULT);
+
 
 	// Run registration code
 	if (isset($_POST['regBtn'])) {
@@ -36,7 +38,7 @@
 
 				} else {
 
-					$query = "INSERT INTO `users` (`email`, `password`) VALUES('".$email."', '".$password."')";
+					$query = "INSERT INTO `users` (`email`, `password`) VALUES('".$email."', '".$hash."')";
 
 					if (mysqli_query($link, $query)) {
 						$message = 'Signed up successfully!';
@@ -73,19 +75,34 @@
 
 			} else {
 
-				$query = "SELECT `id` FROM `users` WHERE email = '".$email."' AND password = '".$password."'";
+				$query = "SELECT `*` FROM `users` WHERE email = '".$email."'";
 
 				$result = mysqli_query($link, $query);
 
+				print_r($result);
+				echo $hash;
+
 				if (mysqli_num_rows($result) > 0) {
 
-					if (isset($_POST['checkbox'])) {
-						setcookie('loggedIn', '', time() + 60 * 60 * 24 * 365);
+					while ($row = mysqli_fetch_array($result)) {
+
+						$dbPassword = $row['password'];
 
 					}
-					$_SESSION['email'] = $email;
-					$_SESSION['loggedIn'] = true;
-					header('Location: diary.php');
+
+					if (password_verify($password, $dbPassword)) {
+						if (isset($_POST['checkbox'])) {
+							setcookie('loggedIn', '', time() + 60 * 60 * 24 * 365);
+
+						}
+						$_SESSION['email'] = $email;
+						$_SESSION['loggedIn'] = true;
+						header('Location: diary.php');
+
+					} else {
+						echo "That email doesn't exist or the password is incorrect!";
+					}
+
 
 					// print_r $result;
 
@@ -109,31 +126,61 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
 	<title>Secret Diary</title>
+
+	<!-- Styling -->
+	<style rel="stylesheet" type="text/css" media="screen">
+		.hidden {
+			display: none;
+		}
+	</style>
+
 </head>
 <body>
+	<h1>Secret Diary</h1>
+	<p><strong>Store your thoughts permanently and securely.</strong></p>
 	<div class="">
 		<?php echo $message, $emailErr, $pwErr; ?>
 	</div>
 	<!-- Sign up form -->
-	<form class="" action="" method="post">
+	<form class="" id="registerForm" action="" method="post">
+		<p>Interested? Sign up now.</p>
 		<input type="email" name="email" value="<?php if(isset($_POST['regBtn'])){if(empty($password)){echo $email;}}?>" placeholder="Your email">
 		<input type="password" name="password" value="" placeholder="Password">
 		<input type="checkbox" name="checkbox">
 		<input type="submit" name="regBtn" value="Sign Up!">
+		<a href="#" id="loginLink">Log In</a>
 	</form>
-	
+
 	<!-- Log in form -->
-	<form class="" action="" method="post">
+	<form class="hidden" id="loginForm" action="" method="post">
+		<p>Already have an account? Log in below.</p>
 		<input type="email" name="email" value="<?php if(isset($_POST['logBtn'])){if(empty($password)){echo $email;}}?>" placeholder="Your email">
 		<input type="password" name="password" value="" placeholder="Password">
 		<input type="checkbox" name="checkbox">
 		<input type="submit" name="logBtn" value="Log In!">
+		<a href="#" id="registerLink">Register</a>
 	</form>
 
 
-	<!-- Setup Bootstrap -->
-	<!-- Setup jQuery -->
-	
+	<!-- Bootstrap and jQuery -->
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('form a').click(function () {
+				$('#loginForm').toggleClass('hidden');
+				$('#registerForm').toggleClass('hidden');
+				return false;
+			})
+		});
+
+	</script>
+
 </body>
 </html>
